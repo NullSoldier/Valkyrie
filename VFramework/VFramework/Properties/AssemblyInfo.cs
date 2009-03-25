19d784d81a511d20e6b2076cd813f72f5b2a29fe
nullsoldier <nullsoldier@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-25 00:41:37 +0000

    * Added VFramework  * Added VGame
    git-svn-id: http://subversion.assembla.com/svn/Project_Valkyrie2@1 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ VFramework/VFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VFramework")]
+[assembly: AssemblyProduct("VFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3a88b61-7103-490a-9338-dce5b56e9593")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
