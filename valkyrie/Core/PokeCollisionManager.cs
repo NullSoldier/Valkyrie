e5e85b5bf0b3865dd5bf121ab8ae6f46e3744149
nullsoldier <nullsoldier@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-07-13 08:36:54 +0000

    * Forgot to commit PokeCollisionManager.cs
    git-svn-id: http://subversion.assembla.com/svn/Project_Valkyrie2@13 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ valkyrie/Core/PokeCollisionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ValkyrieLibrary.Collision;
+using ValkyrieLibrary;
+using Microsoft.Xna.Framework;
+
+namespace valkyrie.Core
+{
+	class PokeCollisionManager
+		: CollisionManager
+	{
+
+		public override bool CheckCollision(ICollidable Source, Point Destination)
+		{
+			Point tilePoint = new Point(Destination.X / TileEngine.Map.TileSize.X, Destination.Y / TileEngine.Map.TileSize.Y);
+
+			if (TileEngine.Map.GetCollisionLayerValue(tilePoint) != -1)
+				return false;
+
+
+
+			return true;
+		}
+	}
+}
