2e118fa626d0ff44936c655eef352c035591afbe
nullsoldier <nullsoldier@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-28 03:25:40 +0000

    * Fixed more Gablarski transition issues because the .SVN files were copied from another repo
    git-svn-id: http://subversion.assembla.com/svn/Project_Valkyrie2@102 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ trunk/ValkyrieServerLibrary/Network/Network/SocketValueWriter.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Gablarski.Network
+{
+	public class SocketValueWriter
+		: IValueWriter
+	{
+		public SocketValueWriter (Socket uclient)
+			: this (uclient, 1280)
+		{
+		}
+
+		public SocketValueWriter (Socket uclient, int length)
+		{
+			this.client = uclient;
+			this.EnsureCapacity (length);
+		}
+
+		public SocketValueWriter (Socket uclient, EndPoint sendTo)
+			: this (uclient)
+		{
+			this.endpoint = sendTo;
+		}
+
+		#region Implementation of IDisposable
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		/// <filterpriority>2</filterpriority>
+		public void Dispose()
+		{
+		}
+
+		#endregion
+
+		#region Implementation of IValueWriter
+
+		public void WriteBytes (byte[] value)
+		{
+			WriteInt32 (value.Length);
+
+			EnsureCapacity (this.size + value.Length);
+			Array.Copy (value, 0, this.buffer, this.size, value.Length);
+			this.size += value.Length;
+		}
+
+		public void WriteSByte (sbyte value)
+		{
+			EnsureCapacity (this.size + sizeof(sbyte));
+			this.buffer[this.size++] = (byte)value;
+		}
+
+		public void WriteInt16 (short value)
+		{
+			EnsureCapacity (this.size + sizeof (Int16));
+			
+			Array.Copy (BitConverter.GetBytes (value), 0, this.buffer, this.size, sizeof (Int16));
+			this.size += sizeof (Int16);
+		}
+
+		public void WriteInt32 (int value)
+		{
+			EnsureCapacity (this.size + sizeof (Int32));
+			
+			Array.Copy (BitConverter.GetBytes (value), 0, this.buffer, this.size, sizeof (Int32));
+			this.size += sizeof (Int32);
+		}
+
+		public void WriteInt64 (long value)
+		{
+			EnsureCapacity (this.size + sizeof (Int32));
+			
+			Array.Copy (BitConverter.GetBytes (value), 0, this.buffer, this.size, sizeof (Int32));
+			this.size += sizeof (Int32);
+		}
+
+		public void WriteByte (byte value)
+		{
+			EnsureCapacity (this.size + 1);
+			this.buffer[this.size++] = value;
+		}
+
+		public void WriteUInt16(ushort value)
+		{
+			EnsureCapacity (this.size + sizeof (Int16));
+			
+			Array.Copy (BitConverter.GetBytes (value), 0, this.buffer, this.size, sizeof (Int16));
+			this.size += sizeof (Int16);
+		}
+
+		public void WriteUInt32(uint value)
+		{
+			EnsureCapacity (this.size + sizeof (Int32));
+			
+			Array.Copy (BitConverter.GetBytes (value), 0, this.buffer, this.size, sizeof (Int32));
+			this.size += sizeof (Int32);
+		}
+
+		public void WriteUInt64(ulong value)
+		{
+			EnsureCapacity (this.size + sizeof (Int64));
+			
+			Array.Copy (BitConverter.GetBytes (value), 0, this.buffer, this.size, sizeof (Int64));
+			this.size += sizeof (Int64);
+		}
+
+		public void WriteString (string value)
+		{
+			byte[] str = this.encoding.GetBytes (value);
+			EnsureCapacity (this.size + str.Length);
+
+			Array.Copy (str, 0, this.buffer, this.size, str.Length);
+			this.size += str.Length;
+		}
+
+		public void Flush()
+		{
+			try
+			{
+				lock (client)
+				{
+					client.SendTo (this.buffer, this.size, SocketFlags.None, this.endpoint);
+				}
+			}
+			catch (SocketException)
+			{
+			}
+			finally
+			{
+				this.size = 0;
+			}
+		}
+
+		#endregion
+
+		private readonly Socket client;
+		private readonly Encoding encoding = Encoding.UTF8;
+		private byte[] buffer;
+		private int size;
+		private EndPoint endpoint;
+
+		private void EnsureCapacity (int min)
+		{
+			byte[] tbuffer = this.buffer;
+			if (tbuffer == null)
+			{
+				this.buffer = new byte[min];
+				return;
+			}
+
+			if (tbuffer.Length >= min)
+				return;
+
+			int nlen = tbuffer.Length*2;
+			if (nlen < min)
+				nlen = min;
+
+			byte[] nbuffer = new byte[nlen];
+			if (this.size > 0)
+				Array.Copy (tbuffer, 0, nbuffer, 0, this.size);
+
+			this.buffer = nbuffer;
+		}
+	}
+}
---
+++ trunk/ValkyrieServerLibrary/Network/Server/ConnectionCollection.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gablarski.Messages;
+
+namespace Gablarski.Server
+{
+	internal class ConnectionCollection
+	{
+		public int ConnectionCount
+		{
+			get { return this.connections.Count; }
+		}
+
+		public UserInfo this[IConnection key]
+		{
+			get
+			{
+				lock (lck)
+				{
+					if (this.users.ContainsKey (key))
+						return this.users[key];
+				}
+
+				return null;
+			}
+		}
+
+		public IConnection this[UserInfo key]
+		{
+			get
+			{
+				lock (lck)
+				{
+					return (from kvp in this.users where kvp.Value == key select kvp.Key).FirstOrDefault();
+				}
+			}
+		}
+
+		public KeyValuePair<IConnection, UserInfo> this[int index]
+		{
+			get
+			{
+				lock (lck)
+				{
+					IConnection c = this.connections[index];
+					UserInfo p;
+					this.users.TryGetValue (c, out p);
+
+					return new KeyValuePair<IConnection, UserInfo> (c, p);
+				}
+			}
+		}
+
+		public IEnumerable<UserInfo> Users
+		{
+			get
+			{
+				lock (lck)
+				{
+					UserInfo[] copiedPlayers = new UserInfo[this.users.Count];
+					this.users.Values.CopyTo (copiedPlayers, 0);
+
+					return copiedPlayers;
+				}
+			}
+		}
+
+		public bool UserLoggedIn (string nickname)
+		{
+			lock (lck)
+			{
+				return this.users.Values.Any (p => p.Nickname == nickname);
+			}
+		}
+
+		public void Add (IConnection connection)
+		{
+			lock (lck)
+			{
+				connections.Add (connection);
+			}
+		}
+
+
+		public void Add (IConnection connection, UserInfo user)
+		{
+			lock (lck)
+			{
+				if (!this.connections.Contains (connection))
+					this.connections.Add (connection);
+
+				this.users[connection] = user;
+			}
+		}
+
+		public IConnection GetConnection (object userId)
+		{
+			lock (lck)
+			{
+				return (from kvp in this.users where kvp.Value.UserId.Equals (userId) select kvp.Key).FirstOrDefault();
+			}
+		}
+
+		public bool UpdateIfExists (IConnection connection, UserInfo user)
+		{
+			lock (lck)
+			{
+				if (!this.connections.Contains (connection))
+					return false;
+
+				this.users[connection] = user;
+				return true;
+			}
+		}
+
+		public bool UpdateIfExists (UserInfo user)
+		{
+			lock (lck)
+			{
+				var old = this.users.FirstOrDefault (kvp => kvp.Value.UserId.Equals (user.UserId));
+				if (old.Equals (default(KeyValuePair<IConnection, UserInfo>)))
+					return false;
+
+				this.users[old.Key] = user;
+				return true;
+			}
+		}
+
+		public bool Remove (IConnection connection)
+		{
+			lock (lck)
+			{
+				this.users.Remove (connection);
+				return this.connections.Remove (connection);
+			}
+		}
+
+		public bool Remove (IConnection connection, out int userId)
+		{
+			userId = 0;
+
+			lock (lck)
+			{
+				this.connections.Remove (connection);
+				if (!this.users.ContainsKey (connection))
+					return false;
+
+				var info = this.users[connection];
+				userId = info.UserId;
+				this.users.Remove (connection);
+
+				return true;
+			}
+		}
+
+		public void Send (MessageBase message)
+		{
+			lock (lck)
+			{
+				foreach (IConnection c in this.connections)
+					c.Send (message);
+			}
+		}
+
+		public void Send (MessageBase message, Func<IConnection, bool> selector)
+		{
+			lock (lck)
+			{
+				foreach (IConnection c in this.connections.Where (selector))
+					c.Send (message);
+			}
+		}
+
+		public void Send (MessageBase message, Func<UserInfo, bool> selector)
+		{
+			lock (lck)
+			{
+				foreach (var kvp in this.users)
+				{
+					if (!selector (kvp.Value))
+						continue;
+
+					kvp.Key.Send (message);
+				}
+			}
+		}
+
+		public void Send (MessageBase message, Func<IConnection, UserInfo, bool> selector)
+		{
+			lock (lck)
+			{
+				foreach (var kvp in this.users)
+				{
+					if (!selector (kvp.Key, kvp.Value))
+						continue;
+
+					kvp.Key.Send (message);
+				}
+			}
+		}
+
+		private object lck = new object();
+		private readonly List<IConnection> connections = new List<IConnection>();
+		private readonly Dictionary<IConnection, UserInfo> users = new Dictionary<IConnection, UserInfo>();
+	}
+}
