47da30561693d3f8fd0f8ccc8814069061a27cb7
nullsoldier <nullsoldier@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-28 03:24:06 +0000

    * Added valkyrie messages
    git-svn-id: http://subversion.assembla.com/svn/Project_Valkyrie2@101 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ trunk/ValkyrieServerLibrary/Network/Messages/Message.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gablarski.Messages
+{
+	public enum ClientMessageType
+		: ushort
+	{
+		Connect = 1,
+		Login = 3,
+		Disconnect = 9,
+
+		LocationData = 11,
+		PlayerRequest = 19
+	}
+
+	public enum ServerMessageType
+		: ushort
+	{
+		LoginFailed = 13,
+		LoginSuccess = 15,
+		PlayerUpdate = 17
+	}
+
+	public abstract class Message<TMessage>
+		: MessageBase
+	{
+		protected Message (TMessage messageType)
+		{
+			this.MessageType = messageType;
+		}
+
+		public TMessage MessageType
+		{
+			get;
+			protected set;
+		}
+	}
+
+	//public abstract class DualMessage
+	//    : MessageBase
+	//{
+	//    protected DualMessage (ClientMessage clientMessageType, ServerMessage serverMessageType)
+	//    {
+	//        this.ClientMessageType = clientMessageType;
+	//        this.ServerMessageType = serverMessageType;
+	//    }
+
+	//    public ClientMessage ClientMessageType
+	//    {
+	//        get;
+	//        protected set;
+	//    }
+
+	//    public ServerMessage ServerMessageType
+	//    {
+	//        get;
+	//        protected set;
+	//    }
+	//}
+}
