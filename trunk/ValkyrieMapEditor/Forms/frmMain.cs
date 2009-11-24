829067077d73260dea5c6416e37d17fec97a8323
nullsoldier <nullsoldier@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-11-24 01:03:54 +0000

    * Added saving to Griffin  * Moved assembly to the local Griffin directory
    git-svn-id: http://subversion.assembla.com/svn/Project_Valkyrie2@117 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/ValkyrieMapEditor/Forms/frmMain.cs
+++ trunk/ValkyrieMapEditor/Forms/frmMain.cs
@@ -24,14 +24,7 @@ namespace ValkyrieMapEditor
 {
 	public partial class frmMain : Form
 	{
-		public static Image TileSheetImage;
-		public bool MapChanged = false;
-		public event EventHandler<ScreenResizedEventArgs> ScreenResized;
-		public event EventHandler<ScrollEventArgs> ScrolledMap;
-
-		//private string ValkyrieGameInstallationAssemblyPath = Path.Combine(Directory.GetCurrentDirectory(), "valkyrie.exe");
-		//public static List<Type> EventHandlerTypes;
-		//public static Assembly[] Assemblies;
+		#region Constructors
 
 		public frmMain()
 		{
@@ -41,12 +34,47 @@ namespace ValkyrieMapEditor
 
 			this.pctTileSurface.Initialize();
 			this.pctTileSurface.TileSelectionChanged += this.SelectionChanged;
-
-			//frmMain.Assemblies = new Assembly[] { Assembly.GetEntryAssembly(), Assembly.Load("ValkyrieLibrary"), Assembly.LoadFrom(this.ValkyrieGameInstallationAssemblyPath) };
-			//frmMain.EventHandlerTypes = EventTypeLoader.LoadImplementers<IMapEvent>(frmMain.Assemblies).ToList();			
 		}
 
+		#endregion
 
+		#region Public Properties
+
+		public event EventHandler<ScreenResizedEventArgs> ScreenResized;
+		public event EventHandler<ScrollEventArgs> ScrolledMap;
+
+		#endregion
+
+		#region Public Methods
+
+		public IntPtr getDrawSurface ()
+		{
+			return pctSurface.Handle;
+		}
+
+		public IntPtr getDrawTilesSurface ()
+		{
+			return pctTileSurface.Handle;
+		}
+
+		public bool MapChanged
+		{
+			get { return this.mapchanged; }
+			set
+			{
+				// Add * to title
+
+				this.mapchanged = value;
+			}
+		}
+
+		#endregion
+
+		private bool mapchanged = false;
+		private FileInfo currentmaplocation = null;
+
+		#region UI Events
+		
 		private void frmMain_Load(object sender, EventArgs e)
 		{
 			this.lblVersion.Text = String.Format("Version: {0}", Assembly.GetExecutingAssembly().GetName().Version.ToString());
@@ -62,31 +90,265 @@ namespace ValkyrieMapEditor
 			Application.Exit();
 		}
 
-		public IntPtr getDrawSurface()
+		private void frmMain_Activated (object sender, EventArgs e)
 		{
-			return pctSurface.Handle;
+			MapEditorManager.IgnoreInput = false;
 		}
 
-        public IntPtr getDrawTilesSurface()
-        {
-            return pctTileSurface.Handle;
-        }
+		private void frmMain_Deactivate (object sender, EventArgs e)
+		{
+			MapEditorManager.IgnoreInput = true;
+		}
 
-		private void toolOpen_Click(object sender, EventArgs e)
+		private void toolOpen_Click (object sender, EventArgs e)
 		{
 			OpenFileDialog openDialog = new OpenFileDialog();
 			openDialog.DefaultExt = ".xml";
 			openDialog.Filter = "Valkyrie Maps|*.xml";
-			
+
 			var result = openDialog.ShowDialog(this);
-			if (result == DialogResult.Cancel)
-			    return;
+			if(result == DialogResult.Cancel)
+				return;
 
 			this.LoadMap(new FileInfo(openDialog.FileName));
-        }
+		}
 
-        public void LoadMap(FileInfo MapLocation)
+		private void toolNew_Click (object sender, EventArgs e)
+		{
+			Map newMap = new Map();
+
+			frmProperty dialog = new frmProperty(newMap, true);
+			DialogResult result = dialog.ShowDialog(this);
+			if(result == DialogResult.Cancel)
+				return;
+
+			MapEditorManager.ApplyMapProperties(newMap);
+
+			this.DisplayMapProperties(newMap);
+
+			MapEditorManager.SetCurrentMap(newMap);
+
+			this.currentmaplocation = null;
+		}
+
+		private void toolSave_Click (object sender, EventArgs e)
+		{
+			this.SaveMap();
+		}
+
+		private void toolClose_Click (object sender, EventArgs e)
+		{
+			if(this.MapChanged)
+			{
+				DialogResult result = MessageBox.Show("Do you want to save changes to" + Environment.NewLine, "Save Changes", MessageBoxButtons.YesNoCancel);
+
+				if(result == DialogResult.Yes)
+					this.SaveMap();
+				else if(result == DialogResult.Cancel)
+					return;
+			}
+
+			this.Close();
+		}
+
+		private void btnMapProperties_Click (object sender, EventArgs e)
+		{
+			if(MapEditorManager.CurrentMap != null)
+			{
+				frmProperty dialog = new frmProperty(MapEditorManager.CurrentMap, false);
+				dialog.ShowDialog(this);
+
+				this.DisplayMapProperties(MapEditorManager.CurrentMap);
+
+				MapEditorManager.ApplyMapProperties(MapEditorManager.CurrentMap);
+
+				this.UpdateScrollBars();
+			}
+		}
+
+		private void btnUnderLayer_Click (object sender, EventArgs e)
+		{
+			this.btnUnderLayer.Checked = true;
+
+			this.btnBaseLayer.Checked = false;
+			this.btnMiddleLayer.Checked = false;
+			this.btnTopLayer.Checked = false;
+			this.btnEvent.Checked = false;
+			this.btnCollisionLayer.Checked = false;
+
+			MapEditorManager.CurrentLayer = MapLayers.UnderLayer;
+			MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
+		}
+
+		private void btnBaseLayer_Click (object sender, EventArgs e)
+		{
+			this.btnBaseLayer.Checked = true;
+
+			this.btnUnderLayer.Checked = false;
+			this.btnMiddleLayer.Checked = false;
+			this.btnTopLayer.Checked = false;
+			this.btnEvent.Checked = false;
+			this.btnCollisionLayer.Checked = false;
+
+			MapEditorManager.CurrentLayer = MapLayers.BaseLayer;
+			MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
+		}
+
+		private void btnMiddleLayer_Click (object sender, EventArgs e)
+		{
+			this.btnMiddleLayer.Checked = true;
+
+			this.btnUnderLayer.Checked = false;
+			this.btnBaseLayer.Checked = false;
+			this.btnTopLayer.Checked = false;
+			this.btnEvent.Checked = false;
+			this.btnCollisionLayer.Checked = false;
+
+			MapEditorManager.CurrentLayer = MapLayers.MiddleLayer;
+			MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
+		}
+
+		private void btnTopLayer_Click (object sender, EventArgs e)
+		{
+			this.btnTopLayer.Checked = true;
+
+			this.btnUnderLayer.Checked = false;
+			this.btnBaseLayer.Checked = false;
+			this.btnMiddleLayer.Checked = false;
+			this.btnEvent.Checked = false;
+			this.btnCollisionLayer.Checked = false;
+
+			MapEditorManager.CurrentLayer = MapLayers.TopLayer;
+			MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
+		}
+
+		private void saveAsToolStripMenuItem_Click (object sender, EventArgs e)
+		{
+			this.SaveMapAs();
+		}
+
+		private void SelectionChanged (object sender, TileSelectionChangedEventArgs ev)
+		{
+			MapEditorManager.SelectedTilesRectangle = new Microsoft.Xna.Framework.Rectangle(
+				ev.Selection.X,
+				ev.Selection.Y,
+				ev.Selection.Width,
+				ev.Selection.Height);
+		}
+
+		private void btnHelp_Click (object sender, EventArgs e)
+		{
+			foreach(ToolStripItem item in this.toolStripTools.Items)
+			{
+				if(item.DisplayStyle == ToolStripItemDisplayStyle.Image)
+					item.DisplayStyle = ToolStripItemDisplayStyle.ImageAndText;
+				else
+					item.DisplayStyle = ToolStripItemDisplayStyle.Image;
+			}
+		}
+
+		private void pctSurface_Resize (object sender, EventArgs e)
+		{
+			this.ScreenResized(this, new ScreenResizedEventArgs(this.pctSurface.Size.Width, this.pctSurface.Size.Height));
+			this.UpdateScrollBars();
+		}
+
+		private void TileMap_Scroll (object sender, ScrollEventArgs e)
+		{
+			var handler = this.ScrolledMap;
+
+			if(handler != null)
+				this.ScrolledMap(sender, e);
+		}
+
+		private void btnEvent_Click (object sender, EventArgs e)
+		{
+			this.btnEvent.Checked = true;
+
+			this.btnBaseLayer.Checked = false;
+			this.btnMiddleLayer.Checked = false;
+			this.btnTopLayer.Checked = false;
+			this.btnCollisionLayer.Checked = false;
+
+			if(this.btnEvent.Checked)
+				MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Events);
+			else
+				MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
+		}
+
+		private void btnCollisionLayer_Click (object sender, EventArgs e)
+		{
+			this.btnCollisionLayer.Checked = true;
+
+			this.btnBaseLayer.Checked = false;
+			this.btnMiddleLayer.Checked = false;
+			this.btnTopLayer.Checked = false;
+			this.btnEvent.Checked = false;
+
+			if(this.btnCollisionLayer.Checked)
+				MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Collsion);
+			else
+				MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
+		}
+
+		private void allLayersToolStripMenuItem_Click (object sender, EventArgs e)
+		{
+			this.allLayersToolStripMenuItem.Checked = true;
+
+			currentLayerAndBelowToolStripMenuItem.Checked = false;
+			dimOtherLayersToolStripMenuItem.Checked = false;
+			MapEditorManager.ViewMode = ViewMode.All;
+		}
+
+		private void currentLayerAndBelowToolStripMenuItem_Click (object sender, EventArgs e)
+		{
+			this.currentLayerAndBelowToolStripMenuItem.Checked = true;
+
+			allLayersToolStripMenuItem.Checked = false;
+			dimOtherLayersToolStripMenuItem.Checked = false;
+
+			MapEditorManager.ViewMode = ViewMode.Below;
+		}
+
+		private void dimOtherLayersToolStripMenuItem_Click (object sender, EventArgs e)
+		{
+			this.dimOtherLayersToolStripMenuItem.Checked = true;
+
+			allLayersToolStripMenuItem.Checked = false;
+			currentLayerAndBelowToolStripMenuItem.Checked = false;
+			MapEditorManager.ViewMode = ViewMode.Dim;
+		}
+
+		private void btnViewSelected_Click (object sender, EventArgs e)
+		{
+			this.btnViewSelected.Checked = !this.btnViewSelected.Checked;
+			this.pctTileSurface.DisplayTileSelection = this.btnViewSelected.Checked;
+
+			this.pctTileSurface.Invalidate();
+		}
+
+		private void btnAnimatedTileManager_Click (object sender, EventArgs e)
+		{
+			//throw new NotImplementedException();
+
+			//frmAnimatedTileManager dialog = new frmAnimatedTileManager(TileEngine.CurrentMapChunk);
+			//dialog.ShowDialog(this);
+		}
+
+		private void optionsToolStripMenuItem_Click (object sender, EventArgs e)
+		{
+			frmOptions dialog = new frmOptions();
+			dialog.ShowDialog(this);
+		}
+
+		#endregion
+
+		#region Helper Methods
+
+		private void LoadMap(FileInfo MapLocation)
         {
+			this.currentmaplocation = MapLocation;
+
 			MapEditorManager.GameInstance.Engine.TextureManager.ClearCache();
 			MapEditorManager.GameInstance.Engine.EventProvider.ClearEvents();
 
@@ -147,12 +409,6 @@ namespace ValkyrieMapEditor
 			this.btnEvent.Enabled = enabled;
 		}
 
-		private void pctSurface_Resize(object sender, EventArgs e)
-		{
-			this.ScreenResized(this, new ScreenResizedEventArgs(this.pctSurface.Size.Width, this.pctSurface.Size.Height));
-			this.UpdateScrollBars();
-		}
-
 		private IEnumerable<Assembly> GetAssemblies ()
 		{
 			List<Assembly> assemblies = new List<Assembly>();
@@ -198,270 +454,36 @@ namespace ValkyrieMapEditor
 			    this.VerticalScroll.Value = 0;
 		}
 
-        private void btnMapProperties_Click(object sender, EventArgs e)
-        {
-			if(MapEditorManager.CurrentMap != null)
-			{
-				frmProperty dialog = new frmProperty(MapEditorManager.CurrentMap, false);
-				dialog.ShowDialog(this);
-
-				this.DisplayMapProperties(MapEditorManager.CurrentMap);
-
-				MapEditorManager.ApplyMapProperties(MapEditorManager.CurrentMap);
-
-				this.UpdateScrollBars();
-			}
-        }
-
-		private void btnUnderLayer_Click(object sender, EventArgs e)
-		{
-			this.btnUnderLayer.Checked = true;
-
-			this.btnBaseLayer.Checked = false;
-			this.btnMiddleLayer.Checked = false;
-			this.btnTopLayer.Checked = false;
-			this.btnEvent.Checked = false;
-			this.btnCollisionLayer.Checked = false;
-
-			MapEditorManager.CurrentLayer = MapLayers.UnderLayer;
-			MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
-		}
-
-        private void btnBaseLayer_Click(object sender, EventArgs e)
-        {
-			this.btnBaseLayer.Checked = true;
-
-			this.btnUnderLayer.Checked = false;
-            this.btnMiddleLayer.Checked = false;
-            this.btnTopLayer.Checked = false;
-            this.btnEvent.Checked = false;
-            this.btnCollisionLayer.Checked = false;
-
-			MapEditorManager.CurrentLayer = MapLayers.BaseLayer;
-			MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
-        }
-
-        private void btnMiddleLayer_Click(object sender, EventArgs e)
-        {
-			this.btnMiddleLayer.Checked = true;
-
-			this.btnUnderLayer.Checked = false;
-            this.btnBaseLayer.Checked = false;
-            this.btnTopLayer.Checked = false;
-            this.btnEvent.Checked = false;
-            this.btnCollisionLayer.Checked = false;
-
-			MapEditorManager.CurrentLayer = MapLayers.MiddleLayer;
-			MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
-        }
-
-        private void btnTopLayer_Click(object sender, EventArgs e)
-        {
-			this.btnTopLayer.Checked = true;
-
-			this.btnUnderLayer.Checked = false;
-            this.btnBaseLayer.Checked = false;
-            this.btnMiddleLayer.Checked = false;
-            this.btnEvent.Checked = false;
-            this.btnCollisionLayer.Checked = false;
-
-			MapEditorManager.CurrentLayer = MapLayers.TopLayer;
-			MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
-        }
-
-        private void toolNew_Click(object sender, EventArgs e)
-        {
-            Map newMap = new Map();
-
-            frmProperty dialog = new frmProperty(newMap, true);
-            DialogResult result = dialog.ShowDialog(this);
-			if (result == DialogResult.Cancel)
-				return;
-
-			MapEditorManager.ApplyMapProperties(newMap);
-
-			this.DisplayMapProperties(newMap);
-
-			MapEditorManager.SetCurrentMap(newMap);		
-        }
-
-		private void toolSave_Click(object sender, EventArgs e)
-		{
-			this.SaveMap();
-		}
-
-		private void toolClose_Click(object sender, EventArgs e)
-		{
-			if(this.MapChanged)
-			{
-				DialogResult result = MessageBox.Show("Do you want to save changes to" + Environment.NewLine, "Save Changes", MessageBoxButtons.YesNoCancel);
-
-				if (result == DialogResult.Yes)
-					this.SaveMap();
-				else if (result == DialogResult.Cancel)
-					return;
-			}
-
-			this.Close();
-		}
-
-		private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			this.SaveMapAs();
-		}
-
 		private void SaveMap()
 		{
 			if(MapEditorManager.CurrentMap == null) return;
 
-			//if (MapEditorManager.CurrentMapLocation == null || !MapEditorManager.CurrentMapLocation.Exists)
-			//    this.SaveMapAs();
-			//else
-			//    MapEditorManager.SaveMap(TileEngine.CurrentMapChunk, MapEditorManager.CurrentMapLocation);
+			if(this.currentmaplocation == null || !this.currentmaplocation.Exists)
+			    this.SaveMapAs();
+			else
+				MapEditorManager.SaveMap(MapEditorManager.CurrentMap, this.currentmaplocation);
 		}
 
 		private void SaveMapAs()
 		{
-			//if (TileEngine.IsMapLoaded)
-			//{
-			//    SaveFileDialog dialog = new SaveFileDialog();
-			//    dialog.DefaultExt = ".xml";
-			//    dialog.Filter = "Valkyrie Maps|*.xml";
+			if(MapEditorManager.CurrentMap == null) return;
 
-			//    var result = dialog.ShowDialog();
+			SaveFileDialog dialog = new SaveFileDialog();
+			dialog.DefaultExt = ".xml";
+			dialog.Filter = "Valkyrie Maps|*.xml";
 
-			//    if (result == DialogResult.None || result == DialogResult.Cancel)
-			//        return;
+			var result = dialog.ShowDialog();
 
-			//    FileInfo file = new FileInfo(dialog.FileName);
+			if(result == DialogResult.None || result == DialogResult.Cancel)
+				return;
 
-			//    MapEditorManager.SaveMap(TileEngine.CurrentMapChunk, file);
-			//}
+			FileInfo file = new FileInfo(dialog.FileName);
+
+			MapEditorManager.SaveMap(MapEditorManager.CurrentMap, file);
 		}
 
-		private void btnHelp_Click(object sender, EventArgs e)
-		{
-			foreach (ToolStripItem item in this.toolStripTools.Items)
-			{
-				if (item.DisplayStyle == ToolStripItemDisplayStyle.Image)
-					item.DisplayStyle = ToolStripItemDisplayStyle.ImageAndText;
-				else
-					item.DisplayStyle = ToolStripItemDisplayStyle.Image;
-			}
-		}
-
-		public void SelectionChanged(object sender, TileSelectionChangedEventArgs ev)
-		{
-			MapEditorManager.SelectedTilesRectangle = new Microsoft.Xna.Framework.Rectangle (
-			    ev.Selection.X,
-			    ev.Selection.Y,
-			    ev.Selection.Width,
-			    ev.Selection.Height);
-		}
-
-		private void pctTileSurface_MouseClick(object sender, MouseEventArgs e)
-		{
-
-		}
-
-		private void frmMain_Activated(object sender, EventArgs e)
-		{
-			MapEditorManager.IgnoreInput = false;
-		}
-
-		private void frmMain_Deactivate(object sender, EventArgs e)
-		{
-			MapEditorManager.IgnoreInput = true;
-		}
-
-		private void TileMap_Scroll(object sender, ScrollEventArgs e)
-		{
-			var handler = this.ScrolledMap;
-			
-			if( handler != null)
-				this.ScrolledMap(sender, e);
-		}
-
-        private void btnEvent_Click(object sender, EventArgs e)
-        {
-			this.btnEvent.Checked = true;
-
-            this.btnBaseLayer.Checked = false;
-            this.btnMiddleLayer.Checked = false;
-            this.btnTopLayer.Checked = false;
-            this.btnCollisionLayer.Checked = false;
-
-            if (this.btnEvent.Checked)
-                MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Events);
-            else
-                MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
-        }
-
-        private void btnCollisionLayer_Click(object sender, EventArgs e)
-        {
-			this.btnCollisionLayer.Checked = true;
-
-            this.btnBaseLayer.Checked = false;
-            this.btnMiddleLayer.Checked = false;
-            this.btnTopLayer.Checked = false;
-            this.btnEvent.Checked = false;
-
-            if (this.btnCollisionLayer.Checked)
-                MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Collsion);
-            else
-                MapEditorManager.GameInstance.SwitchToComponent(ComponentID.Draw);
-        }
-
-        private void allLayersToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-			this.allLayersToolStripMenuItem.Checked = true;
-
-            currentLayerAndBelowToolStripMenuItem.Checked = false;
-            dimOtherLayersToolStripMenuItem.Checked = false;
-			MapEditorManager.ViewMode = ViewMode.All;
-        }
-
-        private void currentLayerAndBelowToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-			this.currentLayerAndBelowToolStripMenuItem.Checked = true;
-
-            allLayersToolStripMenuItem.Checked = false;
-            dimOtherLayersToolStripMenuItem.Checked = false;
-
-			MapEditorManager.ViewMode = ViewMode.Below;
-        }
-
-        private void dimOtherLayersToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-			this.dimOtherLayersToolStripMenuItem.Checked = true;
-
-            allLayersToolStripMenuItem.Checked = false;
-            currentLayerAndBelowToolStripMenuItem.Checked = false;
-			MapEditorManager.ViewMode = ViewMode.Dim;
-        }
-
-		private void btnViewSelected_Click(object sender, EventArgs e)
-		{
-			this.btnViewSelected.Checked = !this.btnViewSelected.Checked;
-			this.pctTileSurface.DisplayTileSelection = this.btnViewSelected.Checked;
-
-			this.pctTileSurface.Invalidate();
-		}
-
-		private void btnAnimatedTileManager_Click(object sender, EventArgs e)
-		{
-			throw new NotImplementedException();
-
-			//frmAnimatedTileManager dialog = new frmAnimatedTileManager(TileEngine.CurrentMapChunk);
-			//dialog.ShowDialog(this);
-		}
-
-		private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frmOptions dialog = new frmOptions();
-			dialog.ShowDialog(this);
-		}
-    }
+		#endregion
+	}
 
 
 	#region EventArgs
