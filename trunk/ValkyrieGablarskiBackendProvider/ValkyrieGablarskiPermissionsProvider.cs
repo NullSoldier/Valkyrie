5c1821a9c4aef0eb18bfc72fde29b7125b918292
ermau <ermau@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-12-22 04:36:20 +0000

    * Added Gablarski server backend providers
    git-svn-id: http://subversion.assembla.com/svn/Project_Valkyrie2@152 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ trunk/ValkyrieGablarskiBackendProvider/ValkyrieGablarskiPermissionsProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gablarski;
+using Gablarski.Server;
+
+namespace Valkyrie.Gablarski
+{
+	public class ValkyrieGablarskiPermissionsProvider
+		: IPermissionsProvider
+	{
+		public event EventHandler<PermissionsChangedEventArgs> PermissionsChanged;
+
+		public bool UpdatedSupported
+		{
+			get { return false; }
+		}
+
+		public IEnumerable<Permission> GetPermissions(int userId)
+		{
+			return GetNamesAsPermissions (PermissionName.Login, PermissionName.RequestSource, PermissionName.SendAudio,
+			                              PermissionName.SendAudioToMultipleTargets);
+		}
+
+		public void SetPermissions (int userId, IEnumerable<Permission> permissions)
+		{
+			throw new NotSupportedException();
+		}
+
+		private static IEnumerable<Permission> GetNamesAsPermissions (params PermissionName[] names)
+		{
+			for (int i = 0; i < names.Length; ++i)
+				yield return new Permission (names[i], true);
+		}
+	}
+}
