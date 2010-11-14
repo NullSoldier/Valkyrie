50cbfa5353eee54c3230fdbc4a11c618b50b00bd
nullsoldier <nullsoldier@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-11-14 00:22:21 +0000

    * Added a modulenotloaded exception
    git-svn-id: http://subversion.assembla.com/svn/Project_Valkyrie2@193 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ trunk/src/ValkyrieEngine/Modules/ModuleNotLoadedException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Valkyrie.Engine.Modules
+{
+    public class ModuleNotLoadedException
+        : Exception
+    {
+        public ModuleNotLoadedException(IModule module)
+            : base ("The module " + module.Name + " was not loaded.")
+        {
+            this.Module = module;
+        }
+
+        public IModule Module
+        {
+            get;
+            private set;
+        }
+    }
+}
